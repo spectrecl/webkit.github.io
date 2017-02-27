f2dc2620746dfc97695c6c3a1fa84e38a9b57fc2
spectrecl <ksusha.golov4anskaja@yandex.ru>
2017-02-27 18:04:05 +0300

    first commit

---
+++ sendfooter.php
@@ -0,0 +1,5 @@
+<?php
+$random = mt_rand(10000, 100000 );
+$result = mail("sergiokiseliov@gmail.com", "Консультация#$random",  "Имя: $_POST[name] \nЭлектронная почта: $_POST[email] \nСообщение: $_POST[skype]");
+echo 'Спасибо, в ближайшее время с Вами свяжется наш менеджер для уточнения всех данных';
+?>	
